19cdb0fd6c25f39fed6c3ba918071c2d94cff780
Clemens Fiedler <clemens.mb.fiedler@gmail.com>
2021-08-18 13:38:19 +0200

    - added functionality to download list of all apps and search in them - added code to nicely format an achievement list

--- achievement_tools.py
+++ achievement_tools.py
@@ -6,6 +6,13 @@ __email__ = "clemens.mb.fiedler@gmail.com"
 # imports
 import requests
 from lxml import html
+import json
+import os
+
+from datetime import datetime as dt
+
+# internal imports
+from app_id_db import appid_db
 
 def get_achievement_stats(appid):
     """Download global achievement statistics for a given game.
@@ -44,3 +51,13 @@ def get_achievement_stats(appid):
                 }
         )
     return achievement_list
+
+    def format_achievement_list(achievement_list):
+        """Format a game's list of achievements nicely."""
+        out_string = '\n'.join(
+            [
+                f"- {ac['description']} ({ac['title']})"
+                    for ac in achievement_list
+                ]
+            )
+        print(out_string)
